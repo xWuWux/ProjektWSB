543ae09bba884398c23f6b081f12ab99bbde43db
natalia <chyla.natalia@gmail.com>
2019-12-21 13:21:17 +0100

    Menu

---
+++ projekt.py
@@ -0,0 +1,10 @@
+def menu():
+    print("1. Pobierz plik z internetu ")
+    print("2. Zlicz liczbe liter w pobranym pliku ")
+    print("3. Zlicz liczbe wyrazow w pliku ")
+    print("4. Zlicz liczbe znakow interpunkcyjnych w pliku ")
+    print("5. Zlicz liczbe zdan w pliku")
+    print("6. Wygeneruj raport o uzyciu liter (A-Z) ")
+    print("7. Zapisz statystyki z punktow 2-5 do pliku statystyki.txt ")
+    print("8. Wyjscie z programu ")
+    choice= int(input("Enter choice: "))
