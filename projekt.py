c6d2164c20ef599cfce2c0d0fdeff2623ab246bb
natalia <chyla.natalia@gmail.com>
2020-01-06 15:38:57 +0100

    menu loop

--- projekt.py
+++ projekt.py
@@ -12,89 +12,99 @@ def menu():
     choice = int(input("Enter choice: "))
     return choice
 
-choice = menu()
-
-if choice==1:
-    url = "http://s3.zylowski.net/public/input/5.txt"
-    r = requests.get(url)
-    with open('file.txt', 'w') as file:
-        file.write(r.text.encode('UTF-8'))
-
-elif choice == 3:
-    plik = open('file.txt')
-    try:
-        tekst = plik.read()
-    finally:
-        plik.close()
-
-    data = tekst.split(" ")
-    num_of_words = len(data)
-    print('Count in text file :', num_of_words)
-elif choice == 2:
-    def count_letters():
-        global letters
-        letters = 0
+while True:
+    menu()
+    while True:
+        choice = int(input("Enter choice: "))
         try:
-            with open(filename, 'r') as myfile:
-                data = myfile.read()
+            if choice >=1 and choice <=8:
+                break
+        except ValueError:
+            choice = -1
 
-            for x in data:
-                if x.isalpha():
-                    letters += 1
+        print("Please select good value \n")
 
-            print(" Ilość liter w pliku ", filename, " to ", str(letters))
+    if choice==1:
+        url = "http://s3.zylowski.net/public/input/5.txt"
+        r = requests.get(url)
+        with open('file.txt', 'w') as file:
+            file.write(r.text.encode('UTF-8'))
+
+    elif choice == 3:
+        plik = open('file.txt')
+        try:
+            tekst = plik.read()
+        finally:
+            plik.close()
+
+        data = tekst.split(" ")
+        num_of_words = len(data)
+        print('Count in text file :', num_of_words)
+    elif choice == 2:
+        def count_letters():
+            global letters
+            letters = 0
+            try:
+                with open(filename, 'r') as myfile:
+                    data = myfile.read()
+
+                for x in data:
+                    if x.isalpha():
+                        letters += 1
+
+                print(" Ilość liter w pliku ", filename, " to ", str(letters))
+            except FileNotFoundError:
+                print(" ** Brak pliku ", filename, " **")
+            except Exception:
+                print(" ** Nie mogę otworzyć pliku ", filename)
+    elif choice == 4:
+        plik = open('file.txt')
+        try:
+            tekst = plik.read()
+        finally:
+            plik.close()
+
+        dataD = tekst.split(".")
+        dataC = tekst.split(",")
+        dataS = tekst.split(";")
+        dataCo = tekst.split(":")
+        dataQ = tekst.split("?")
+        dataE = tekst.split("!")
+        dataDa = tekst.split("-")
+
+        num_of_d = len(dataD)
+        num_of_c = len(dataC)
+        num_of_s = len(dataS)
+        num_of_co = len(dataCo)
+        num_of_q = len(dataQ)
+        num_of_e = len(dataE)
+        num_of_Da = len(dataDa)
+
+
+        print('. : ',num_of_d)
+        print(', : ',num_of_c)
+        print('; : ',num_of_s)
+        print(': : ',num_of_co)
+        print('? : ',num_of_q)
+        print('! : ',num_of_e)
+        print('- : ',num_of_Da)
+
         except FileNotFoundError:
             print(" ** Brak pliku ", filename, " **")
         except Exception:
             print(" ** Nie mogę otworzyć pliku ", filename)
-elif choice == 4:
-    plik = open('file.txt')
-    try:
-        tekst = plik.read()
-    finally:
-        plik.close()
 
-    dataD = tekst.split(".")
-    dataC = tekst.split(",")
-    dataS = tekst.split(";")
-    dataCo = tekst.split(":")
-    dataQ = tekst.split("?")
-    dataE = tekst.split("!")
-    dataDa = tekst.split("-")
-
-    num_of_d = len(dataD)
-    num_of_c = len(dataC)
-    num_of_s = len(dataS)
-    num_of_co = len(dataCo)
-    num_of_q = len(dataQ)
-    num_of_e = len(dataE)
-    num_of_Da = len(dataDa)
+        menu()
+    elif choice == 5:
+        plik = open('file.txt')
+        try:
+            tekst = plik.read()
+        finally:
+            plik.close()
 
 
-    print('. : ',num_of_d)
-    print(', : ',num_of_c)
-    print('; : ',num_of_s)
-    print(': : ',num_of_co)
-    print('? : ',num_of_q)
-    print('! : ',num_of_e)
-    print('- : ',num_of_Da)
-
-    except FileNotFoundError:
-        print(" ** Brak pliku ", filename, " **")
-    except Exception:
-        print(" ** Nie mogę otworzyć pliku ", filename)
-        
-    menu()
-elif choice == 5:
-    plik = open('file.txt')
-    try:
-        tekst = plik.read()
-    finally:
-        plik.close()
-
-
-        data = tekst.split(".")
-        num_of_d = len(data)
-        print('Count in text file :', num_of_d)
+            data = tekst.split(".")
+            num_of_d = len(data)
+            print('Count in text file :', num_of_d)
 
 
